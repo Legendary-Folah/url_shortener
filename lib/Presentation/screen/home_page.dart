78cd4ce11a1fe603f2f79ff1c83f7fa12a4cc5b9
FolaDev <folarinfadeyi@gmail.com>
2024-11-28 11:11:09 +0100

    adder persistent bottom nav bar package to enable animated nav bar and also to learn from it and it seems quite easy and good

--- lib/Presentation/screen/home_page.dart
+++ lib/Presentation/screen/home_page.dart
@@ -1,5 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:persistent_bottom_nav_bar/persistent_bottom_nav_bar.dart';
 import 'package:url_shortener_project/Core/colors.dart';
+import 'package:url_shortener_project/Presentation/screen/url_home.dart';
+import 'package:url_shortener_project/Presentation/screen/url_list.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -8,25 +12,80 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+PersistentTabController controller = PersistentTabController(initialIndex: 0);
+
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: ColorsConst.grey,
+    return PersistentTabView(
+      context,
+      controller: controller,
+      screens: _buildScreens(),
+      items: _navBarsItems(),
+      handleAndroidBackButtonPress: true, // Default is true.
+      resizeToAvoidBottomInset:
+          true, // This needs to be true if you want to move up the screen on a non-scrollable screen when keyboard appears. Default is true.
+      stateManagement: true, // Default is true.
+      hideNavigationBarWhenKeyboardAppears: true,
+      popBehaviorOnSelectedNavBarItemPress: PopBehavior.all,
+      padding: const EdgeInsets.only(top: 2),
+      backgroundColor: ColorsConst.white,
+      isVisible: true,
+      animationSettings: const NavBarAnimationSettings(
+        navBarItemAnimation: ItemAnimationSettings(
+          // Navigation Bar's items animation properties.
+          duration: Duration(milliseconds: 400),
+          curve: Curves.ease,
+        ),
+        screenTransitionAnimation: ScreenTransitionAnimationSettings(
+          // Screen transition animation on change of selected tab.
+          animateTabTransition: true,
+          duration: Duration(milliseconds: 200),
+          screenTransitionAnimationType: ScreenTransitionAnimationType.fadeIn,
+        ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            'You have pushed the button this many times:',
-          ),
-          Text(
-            'Hello',
-            style: Theme.of(context).textTheme.headlineMedium,
-          ),
-        ],
-      ),
+      confineToSafeArea: true,
+      navBarHeight: kBottomNavigationBarHeight,
+      navBarStyle:
+          NavBarStyle.style1, // Choose the nav bar style with this property
     );
   }
+}
+
+List<Widget> _buildScreens() {
+  return [
+    const UrlHome(),
+    const UrlList(),
+  ];
+}
+
+List<PersistentBottomNavBarItem> _navBarsItems() {
+  return [
+    PersistentBottomNavBarItem(
+      icon: const Icon(CupertinoIcons.home),
+      title: ("Home"),
+      activeColorPrimary: CupertinoColors.black,
+      inactiveColorPrimary: CupertinoColors.systemPurple,
+      routeAndNavigatorSettings: RouteAndNavigatorSettings(
+        initialRoute: "/",
+        routes: {
+          "/first": (final context) => const UrlHome(),
+          "/second": (final context) => const UrlList(),
+        },
+      ),
+    ),
+    PersistentBottomNavBarItem(
+      icon: const Icon(CupertinoIcons.link),
+      title: ("Links"),
+      activeColorPrimary: CupertinoColors.black,
+      inactiveColorPrimary: CupertinoColors.systemPurple,
+      routeAndNavigatorSettings: RouteAndNavigatorSettings(
+        initialRoute: "/",
+        routes: {
+          "/first": (final context) => const UrlHome(),
+          "/second": (final context) => const UrlList(),
+        },
+      ),
+    ),
+  ];
 }
