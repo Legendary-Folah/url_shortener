d70a015366304782ba4d745088d95a19de0492cf
FolaDev <folarinfadeyi@gmail.com>
2024-11-29 00:07:43 +0100

    initialized firebase in the project and added the dependencies. Continue tomorrow

--- lib/Data/firebase_service.dart
+++ lib/Data/firebase_service.dart
@@ -3,5 +3,22 @@ import 'package:uuid/uuid.dart';
 
 class FirebaseService {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  final uuid = const Uuid().v4();
+
+  String listCollection = 'links';
+
+  Future<bool?> addList(String? link) async {
+    try {
+      final uuid = const Uuid().v4();
+      final date = DateTime.now();
+      await _firestore.collection(listCollection).doc(uuid).set({
+        'link': link,
+        'id': uuid,
+        'time': '${date.hour} : ${date.minute}',
+      });
+      return true;
+    } catch (e) {
+      print('Error adding link: $e');
+      return true;
+    }
+  }
 }
