22a782d095b71993c962116aad2a236e7016d6f2
FolaDev <folarinfadeyi@gmail.com>
2025-01-16 14:46:24 +0100

    replaced bitly with tinyurl to shorten links

--- lib/Data/firebase/firebase_service.dart
+++ lib/Data/firebase/firebase_service.dart
@@ -44,7 +44,7 @@ class FirebaseService {
 
   Future<String> shortenURLWithTiny(String longUrl) async {
     String accessToken = Constants.tinyUrlAccess;
-    String apiUrl = Constants.tinyUrl;
+    String apiUrl = 'https://api.tinyurl.com/create';
     try {
       final response = await http.post(
         Uri.parse(apiUrl),
