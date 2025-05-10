90f9486ba54a337306d4b36f94bb81732a10f6ba
FolaDev <folarinfadeyi@gmail.com>
2025-05-10 10:10:18 +0100

    bottom Nav bar

--- lib/Presentation/screen/home_page.dart
+++ lib/Presentation/screen/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:persistent_bottom_nav_bar/persistent_bottom_nav_bar.dart';
+import 'package:url_shortener_project/Core/colors.dart';
 import 'package:url_shortener_project/Presentation/screen/url_home.dart';
 import 'package:url_shortener_project/Presentation/screen/url_list.dart';
 
@@ -10,11 +11,34 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-PersistentTabController controller = PersistentTabController(initialIndex: 0);
-
 class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+
+  final List _screens = [
+    const UrlHome(),
+    const UrlList(),
+  ];
+
+  void onTap(value) {
+    setState(() {
+      _currentIndex = value;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: ColorsConst.white,
+      body: _screens.elementAt(_currentIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: ColorsConst.white,
+        type: BottomNavigationBarType.fixed,
+        onTap: onTap,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.list), label: 'List')
+        ],
+      ),
+    );
   }
 }
